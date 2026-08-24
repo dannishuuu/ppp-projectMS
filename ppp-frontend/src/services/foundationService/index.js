aed6b1a65ee847b7c368f0d4d4e50da40c163e27
dannishuuu <danielgelandu1@gmail.com>
2026-08-24 15:13:06 +0300

    change on foundation service index

--- ppp-frontend/src/services/foundationService/index.js
+++ ppp-frontend/src/services/foundationService/index.js
@@ -2,3 +2,6 @@ export * from './currencyService';
 export * from './proposalStatusService';
 export * from './projectStatusService';
 export * from './countriesService';
+export * from './regionsService';
+export * from './woredasService';
+export * from './zonesService';
